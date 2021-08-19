501d00d1be1a85576f5b8bb4865ed855cac779a9
Elvia <etcamarena@gmail.com>
2021-08-19 13:17:40 -0700

    changed the folio links to index path

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/folio.html",
+      template: "./src/index.html",
       favicon: "./src/assets/favicon.png",
     }),
   ],
