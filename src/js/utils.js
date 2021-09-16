5087c6910066696dbbf6a7b2ccb6313b8063f4d2
Elvia <etcamarena@gmail.com>
2021-09-15 18:58:32 -0700

    cleaned up &pointed buttons and links to the right path. added juicybliss project.

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,5 +2,5 @@ export function addResume(pdf) {
   if (!pdf) return;
 
   const resumeButton = document.querySelector(".cta-btn--resume");
-  resumeButton.setAttribute("href", pdf);
+  resumeButton.setAttribute("href", "https://www.juicybliss.com/download_resume.pdf");
 }
